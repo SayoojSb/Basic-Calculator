57b2b0cfa59e54f8fe007f34e01bdb15c0d01a1c
SayoojSB <sayoojsb104@gmail.com>
2025-03-15 12:51:44 +0530

    Added file via upload
    Made a basic calculator as a part of my journey of learning javascript.

---
+++ index.js
@@ -0,0 +1,29 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let result = document.getElementById("sum-el")
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+function summ(){
+    res = num1+num2 
+    result.textContent = res
+}
+function sub(){
+    res = num1-num2 
+    result.textContent = res
+}
+function divi(){
+    res = num1/num2 
+    result.textContent = res
+}
+function mult(){
+    res = num1*num2 
+    result.textContent = res
+}
